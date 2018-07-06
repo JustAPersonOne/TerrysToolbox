6f07ac56c4017a14dd0f4886d4bf716af4ac5f76
JustAPersonOne <justapersonone@gmail.com>
2018-07-06 16:13:56 +0100

    undo

--- src/main/java/terranceyoung/plugin/terrystoolbox/commands/SchematicExecutor.java
+++ src/main/java/terranceyoung/plugin/terrystoolbox/commands/SchematicExecutor.java
@@ -3,11 +3,9 @@ package terranceyoung.plugin.terrystoolbox.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import terranceyoung.plugin.terrystoolbox.schematics.Schematic;
 
 public class SchematicExecutor implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-
         return true;
     }
 }
